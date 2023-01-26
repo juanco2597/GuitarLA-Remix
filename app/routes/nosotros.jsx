c1dbfc86852976ebdb393d1538be52b8dd1979cd
Juan Manuel Parra <39108044+juanco2597@users.noreply.github.com>
2023-01-26 13:05:47 -0500

    Pagina Nosotros con perfomance

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,4 +1,27 @@
 import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
+
+
+export function meta() {
+    return {
+        title: 'GuitarLA - Sobre Nosotros',
+        dercription: 'Venta de guitarras, blog de musica'
+    }
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        },
+        {
+            rel: 'preload',
+            href: imagen,
+            as: 'image'
+        }
+    ]
+}
 
 function Nosotros() {
     return (
@@ -6,12 +29,12 @@ function Nosotros() {
             <h2 className="heading">Nosotros</h2>
 
             <div className="contenido">
-                <img src={imagen} alt="" />
+                <img src={imagen} alt="Guitarra" />
 
                 <div>
-                    <p>Aenean quis tellus quis elit accumsan molestie. Donec vel rhoncus est. Donec maximus ante id est auctor, vel tincidunt velit porttitor. Praesent finibus ullamcorper augue, eu ultrices orci fermentum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut faucibus magna vitae faucibus iaculis. Fusce sit amet commodo justo. Cras bibendum vel dui nec dignissim. Donec commodo ipsum ut semper bibendum. Integer vehicula aliquam massa, eu gravida massa tristique eget. Curabitur ut tellus elit. Pellentesque accumsan consequat mi, eu facilisis magna consectetur in. Quisque viverra pretium sodales. Aenean ac lorem ligula.</p>
+                    <p className='text'>Aenean quis tellus quis elit accumsan molestie. Donec vel rhoncus est. Donec maximus ante id est auctor, vel tincidunt velit porttitor. Praesent finibus ullamcorper augue, eu ultrices orci fermentum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut faucibus magna vitae faucibus iaculis. Fusce sit amet commodo justo. </p>
 
-                    <p>Aenean quis tellus quis elit accumsan molestie. Donec vel rhoncus est. Donec maximus ante id est auctor, vel tincidunt velit porttitor. Praesent finibus ullamcorper augue, eu ultrices orci fermentum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut faucibus magna vitae faucibus iaculis. Fusce sit amet commodo justo. Cras bibendum vel dui nec dignissim. Donec commodo ipsum ut semper bibendum. Integer vehicula aliquam massa, eu gravida massa tristique eget. Curabitur ut tellus elit. Pellentesque accumsan consequat mi, eu facilisis magna consectetur in. Quisque viverra pretium sodales. Aenean ac lorem ligula.</p>
+                    <p className='text'>Aenean quis tellus quis elit accumsan molestie. Donec vel rhoncus est. Donec maximus ante id est auctor, vel tincidunt velit porttitor. Praesent finibus ullamcorper augue, eu ultrices orci fermentum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut faucibus magna vitae faucibus iaculis. Fusce sit amet commodo justo. </p>
                 </div>
             </div>
         </main>
